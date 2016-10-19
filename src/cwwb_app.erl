b6717c08d67892c1126eeae50346a8b23c7c4647
Petr Severa <segedacz@gmail.com>
2016-10-19 10:37:10 +0200

    PORT env

--- src/cwwb_app.erl
+++ src/cwwb_app.erl
@@ -8,10 +8,19 @@ start(_Type, _Args) ->
     Dispatch = cowboy_router:compile([
         {'_', [{"/", cwwb_handler, []}]}
     ]),
-    cowboy:start_http(my_http_listener, 100, [{port, 8080}],
+    cowboy:start_http(my_http_listener, 100, [{port, port()}],
         [{env, [{dispatch, Dispatch}]}]
     ),
     cwwb_sup:start_link().
 
 stop(_State) ->
 	ok.
+
+port() ->
+    case os:getenv("PORT") of
+        false ->
+            {ok, Port} = application:get_env(http_port),
+            Port;
+        Other ->
+            list_to_integer(Other)
+    end.
